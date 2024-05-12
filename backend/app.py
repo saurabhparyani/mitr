bb27a1c916e078522d7eedbd333575bf539096d0
Saurabh Paryani <saurabhparyani64@gmail.com>
2024-05-13 04:02:08 +0530

    added cors

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
