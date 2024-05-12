33ba6357f03592e0b5b27c2206610a8cdcba4f53
Saurabh Paryani <saurabhparyani64@gmail.com>
2024-05-13 04:55:47 +0530

    changed be

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
+# TODO: UPDATE THIS FILE FOR DEPLOYMENT
 from flask import Flask, send_from_directory
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+
 CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
@@ -15,7 +16,7 @@ db = SQLAlchemy(app)
 frontend_folder = os.path.join(os.getcwd(),"..","frontend")
 dist_folder = os.path.join(frontend_folder,"dist")
 
-# Serve static files from the dist folder under the frontend directory
+# Server static files from the "dist" folder under the "frontend" directory
 @app.route("/",defaults={"filename":""})
 @app.route("/<path:filename>")
 def index(filename):
@@ -23,12 +24,11 @@ def index(filename):
     filename = "index.html"
   return send_from_directory(dist_folder,filename)
 
-
 # api routes
 import routes
 
 with app.app_context():
-    db.create_all()
+  db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
